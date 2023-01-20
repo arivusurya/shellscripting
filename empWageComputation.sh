9c6663f13d07a163103cfa9094e29bc412ec2d14
arivusurya <arivusurya.3@gmail.com>
2023-01-20 19:31:40 +0530

    monthly wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,13 +4,14 @@ fulltimeorhalftime=$((RANDOM%2))
 wageperhour=20
 hourperday=8
 parttimehour=6
+daysonmonth=20
 echo $fulltimeorhalftime
 
 case $fulltimeorhalftime in  
 	0 )
-	wages=$(( $wageperhour * $hourperday )) ;;
+	wages=$(( $wageperhour * $hourperday * $daysonmonth )) ;;
 	1 )
-	wages=$(( $wageperhour * $parttimehour )) ;;
+	wages=$(( $wageperhour * $parttimehour * $daysonmonth )) ;;
 	*)
 	wages=0 ;;
 esac
