051f8b0e55356db1a9f2ceda71f9fdea295a324e
arivusurya <arivusurya.3@gmail.com>
2023-01-20 18:00:01 +0530

    welcom display

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,3 @@
 #!/bin/bash -x
 
-Attendence=$((RANDOM%2))
-
-if [ $Attendence -eq 0 ]
-then
-	echo "Absent"
-fi
-if [ $Attendence -eq 1 ]
-then
-	echo "present"
-fi
+echo "WELCOME TO THE WAGE COMPUTATION PROGRAM "
