60542320c0612703c902368c1a3176891cdf5533
arivusurya <arivusurya.3@gmail.com>
2023-01-20 19:25:26 +0530

    switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -x
 
+fulltimeorhalftime=$((RANDOM%2))
 wageperhour=20
-parttime=8
-dailywage=$(( $wageperhour * $parttime ))
+hourperday=8
+parttimehour=6
+echo $fulltimeorhalftime
+
+case $fulltimeorhalftime in  
+	0 )
+	wages=$(( $wageperhour * $hourperday )) ;;
+	1 )
+	wages=$(( $wageperhour * $parttimehour )) ;;
+	*)
+	wages=0 ;;
+esac
+echo $wages
 
